d64c0311fc22f80ffd8a7b6db5e2aa0c5b5aef16
RadmehrAghakhani <radmehraghakhani2003@gmail.com>
2023-12-01 23:46:13 +0330

    changed due to host utility.py and some port added to secondary port list

--- nmap.py
+++ nmap.py
@@ -1,44 +1,8 @@
-import socket
 import sys
 from defined_ports import *
 from port_object import *
+from host_utility import *
 
-
-def check_ports_for_least_popular_ports(target_host, port_list):
-    for port in port_list:
-        host_socket_checker_online = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host_socket_checker_online.settimeout(2)  # timeout for the connection attempt
-        try:
-            # Attempt to connect to the host on the current port
-            host_socket_checker_online.connect((target_host, port))
-            return True  # return to main if connection is successful
-        except (socket.timeout, socket.error):
-            # Connection failed on the current port, try the next one
-            pass
-        finally:
-            host_socket_checker_online.close()
-    return False  # No successful connection on any port, host is offline
-
-
-def check_ports_for_two_first_level_ports(target_host, port_lists):
-    for port_list in port_lists:
-        if port_list == 2:
-            check_ports_for_least_popular_ports(target_host, port_list)
-        for port_object_entity in port_list.prioritized_port_list:
-            port = port_object_entity.prot_number
-            host_socket_checker_online = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host_socket_checker_online.settimeout(2)  # timeout for the  connection attempt
-            try:
-                # Attempt to connect to the host on the current port
-                host_socket_checker_online.connect((target_host, port))
-                return True  # return to main if connection is successful
-            except (socket.timeout, socket.error):
-                # Connection failed on the current port, try the next one
-                pass
-            finally:
-                host_socket_checker_online.close()
-
-    return False  # No successful connection on any port, host is offline
 
 
 def singing_ports_list():
@@ -63,12 +27,16 @@ def singing_ports_list():
         Port_object_entity(8000),
         Port_object_entity(8888),
         Port_object_entity(9090),
-
+        Port_object_entity(1234),
+        Port_object_entity(5678),
+        Port_object_entity(9876),
+        Port_object_entity(3333),
+        Port_object_entity(9999),
     ])
+    return high_priority_ports, medium_priority_ports
 
-    low_priority_ports = range(1024, 49151)
 
-    return high_priority_ports, medium_priority_ports, low_priority_ports
+
 
 
 if __name__ == "__main__":
@@ -78,11 +46,10 @@ if __name__ == "__main__":
 
     host = sys.argv[1]
 
-
     # Define lists of well-known ports, arranged by their priorities
     port_tuple = singing_ports_list()
-
-    if  check_ports_for_two_first_level_ports(host, port_tuple):
+    Host_Scanner = Scanner(host, port_tuple)
+    if Host_Scanner.check_ports_for_two_first_level_ports():
         print(f"{host} is online")
     else:
         print(f"{host} is offline")
