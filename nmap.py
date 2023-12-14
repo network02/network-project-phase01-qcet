a88f43f491c910df029caea3fe9a12888664105f
RadmehrAghakhani <radmehraghakhani2003@gmail.com>
2023-12-14 21:28:58 +0330

    Meanu added for user selecting between scanning ports and HTTP commands simulation

--- nmap.py
+++ nmap.py
@@ -1,9 +1,10 @@
 import sys
-
+import os
 from defined_ports import *
 from port_object import *
 from host_utility import *
 from local_client import *
+
 
 def singing_ports_list():
     high_priority_ports = Well_Known_Ports([
@@ -36,15 +37,7 @@ def singing_ports_list():
     return high_priority_ports, medium_priority_ports
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python nmap.py <host>")
-        sys.exit(1)
-
-    host = sys.argv[1]
-    initial_port = int(sys.argv[2])  # it is string , converting to integer
-    final_port = int(sys.argv[3])
-
+def port_Scanner(host, initial_port, final_port):
     # Define lists of well-known ports, arranged by their priorities
     port_tuple = singing_ports_list()
     Host_Scanner = Scanner(host, port_tuple, initial_port, final_port)
@@ -55,21 +48,92 @@ if __name__ == "__main__":
 
     available_ports_for_target_host = Host_Scanner.scan_range_of_input_ports()
     if len(available_ports_for_target_host) != 0:
-        #print(f"open port detected: {host}   ")
+
         for port in available_ports_for_target_host:
             print(f"open port detected: {host}    --port:{port}     -- service: {Scanner.get_service_port(port)}")
 
     else:
         print("There is no open port detected in range you provide")
 
+    return
 
-while True:
-    print ("Enter 'GET user_id' or 'POS user_name user_age' to simulate a request: ")
-    print ("Enter Zero to quit:")
-    request = input()
-    if request == 0:
-        sys.exit(1)
+
+def GET_POST_METHOD():
+
+    while True:
+        os.system("cls")
+        print("Enter 'GET user_id' or 'POST user_name user_age' to simulate a request: ")
+        print ("0.Exit")
+        request = input()
+        if "GET" in request or "POST" in request:
+            client_request = Client_connection()
+            client_request.intilize_connection_to_server(request)
+            input("Enter to continue")
+        elif request == '0':
+            break
+        else:
+            print ("Bad request. Enter to try again")
+            input()
+
+
+
+    return
+
+
+
+
+def initialize_program(scan_flag = False):
+
+    if scan_flag:
+        while True:
+            os.system("cls")
+            print("please enter the the IP **NOTE DO NOT USE PYTHON AT FIRST**")
+            received_input = input()
+            received_input_split = received_input.split()
+            host = received_input_split[0]
+            initial_port = int(received_input_split[1])
+            final_port = int(received_input_split[2])
+            port_Scanner(host, initial_port, final_port)
+            user_choice = input("To Scan again press 0 otherwise 1 :")
+            if user_choice == '0':
+                pass
+            else:
+                break
     else:
-        client_request = Client_connection()
-        client_request.intilize_connection_to_server(request)
+        GET_POST_METHOD()
 
+    input("Press enter to continue")
+    os.system("cls")
+    return
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 4:
+        print("Usage: python nmap.py <host>")
+        sys.exit(1)
+
+    host = sys.argv[1]
+    initial_port = int(sys.argv[2])  # it is string , converting to integer
+    final_port = int(sys.argv[3])
+    port_Scanner(host, initial_port, final_port)
+    while True:
+        print ("If you want to scan another IP address Enter 0 otherwise press 1 to proceed with HTTP commands simulation:")
+        print("0.Scan IP")
+        print("1.HTTP Commands Simulation")
+        print("2.Exit")
+        try :
+            command = input()
+            if command == '0':
+                initialize_program(True)
+            elif command == '1':
+                initialize_program(False)
+            elif command == '2':
+                print("Exiting the program  ...")
+                break
+            else:
+                raise ValueError("Key wrong pressed")
+        except ValueError as error :
+            os.system("cls")
+            print(f"The Error is {error}")
+            print("Please enter a valid choice")
